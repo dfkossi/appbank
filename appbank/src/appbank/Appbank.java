9547c3fff52444e02c0345f31c03b2b140cefd69
peniel <peniel@peniel-MacBookPro>
2018-02-03 02:04:36 +0700

    Peniel 02-02-2018 : Just a test for the first commit after clone project

---
+++ appbank/src/appbank/Appbank.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appbank;
+
+/**
+ *
+ * @author peniel
+ */
+public class Appbank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //This just a test for the beginning
+    }
+    
+}
