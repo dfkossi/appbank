8b7c00b4c9a8ce115c5f87c86f13894f8aa1dad2
NAT <NAT@DESKTOP-2VNIMEC.connectify>
2018-02-03 18:07:23 +0700

    solim :03/02/2018 18:06 Transaction, Menu,autres méthodes

--- appbank/src/com/gl/appbank/AppBankLauncher.java
+++ appbank/src/com/gl/appbank/AppBankLauncher.java
@@ -49,6 +49,19 @@ public class AppBankLauncher {
         }
     }
 
+    public static void transactionOptions(int entre) {
+        switch (entre) {
+            case 1:
+                depotSurCompte();
+                break;
+            case 2:
+                retraitSurCompte();
+                break;
+            case 3:
+                System.out.println("\n!!! Action annullee. Retour au menu principal !!!");
+        }
+    }
+
     public static void menuGenral() {
         while (true) {
             menuPrincipal();
@@ -75,9 +88,9 @@ public class AppBankLauncher {
                     break;
                 case 2:
                     entree = 0;
-//                    menuTransaction();
+                    menuTransaction();
                     vericationDeLaSaisie(1, 3);
-//                    transactionOptions(entree);
+                    transactionOptions(entree);
 //                    switch (entree) {
 //                        case 1:
 //                            depotSurCompte();
@@ -97,11 +110,35 @@ public class AppBankLauncher {
                 case 4:
                     listerComptes();
                     break;
-                
+                case 5:
+                    listCompteParTitulaire();
+                    break;
+                case 6:
+                    consulterCompte();
+                    break;
+                case 7:
+                    rapportCompte();
+                    break;
+                case 8:
+                    calculInteret();
+                    break;
+                case 9:
+                    mettreFinAuProgramme();
+                    break;
             }
         }
     }
 
+    //verification du titulaire dans la liste
+//    public static boolean titulaireIsExist(int t) {
+//        for (TitulaireCompte tt : listeTitulaireCompte) {
+//            if (t == tt.getNumeroIdTitulaire()) {
+//                return true;
+//            }
+//
+//        }
+//        return false;
+//    }
     private static void accueil() {
         System.out.println("___________________MODULE DE GENIE LOGICIEL AVANCE___________________\n");
 //        System.out.println(" TP1.  MAYABA Solim Wapo\n");
@@ -112,12 +149,14 @@ public class AppBankLauncher {
         System.out.print("\n\n\t\tMENU PRINCIPAL DE GESTION BANCAIRE\n");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
         System.out.print("1 : CREATION D'UN NOUVEAU COMPTE \n");
+        System.out.print("2 : EFFECTUER UNE TRANSACTION SUR UN COMPTE\n");
 //        System.out.print("3 : EFFECTUER UN RETRAIT SUR UN COMPTE\n");
         System.out.print("3 : LISTE DES CLIENTS\n");
         System.out.print("4 : LISTE DES COMPTES\n");
         System.out.print("5 : RECHERCHER COMPTES D'UN TITULAIRE\n");
         System.out.print("6 : CONSULTER SOLDE D'UN COMPTE\n");
         System.out.print("7 : RAPPORT DES COMPTE\n");
+        System.out.print("8 : CALCUL D'INTERET ET MAJ COMPTE\n");
         System.out.print("9 : FIN\n");
         System.out.print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nChoix : ");
     }
@@ -178,7 +217,17 @@ public class AppBankLauncher {
         System.out.print("3 : SORTIR\n");
         System.out.print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nChoix : ");
     }
-    
+
+    //Menu transaction
+    private static void menuTransaction() {
+        System.out.print("\n\n\t\tTRANSACTION SUR UN COMPTE\n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        System.out.print("1 : DEPOT D'ARGENT\n");
+        System.out.print("2 : RETRAIT D'ARGENT\n");
+        System.out.print("3 : SORTIR\n");
+        System.out.print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nChoix : ");
+    }
+
     //Création de compte si le titulaire existe déja
     private static void createCompteTitulaireExist() {
         System.out.print("\n\n~~~CREATION D'UN COMPTE~~~\n\n");
@@ -309,6 +358,22 @@ public class AppBankLauncher {
         return num;
     }
 
+    //Génération automatique du numéro de la transaction
+    public static int genererNumeroTransaction() {
+        int num = 0;
+        if (listeTransactions.isEmpty()) {
+            num = 1;
+        } else {
+            num = listeTransactions.indexOf(0);
+            for (Transaction t : listeTransactions) {
+                if (t.getNumeroTransaction() > num) {
+                    num = t.getNumeroTransaction();
+                }
+            }
+            num++;
+        }
+        return num;
+    }
 
     //Afficher la liste des comptes
     private static void listerComptes() {
@@ -353,5 +418,230 @@ public class AppBankLauncher {
         return null;
     }
 
+    //liste des transactions pour un compte
+    public static List<Transaction> transactionsByCompte(int num) {
+        List<Transaction> l = new ArrayList<>();
+        for (Transaction t : listeTransactions) {
+            if (num == t.getCompte().getNumeroCompte()) {
+                l.add(t);
+            }
+        }
+        return l;
+    }
 
+    //depôt sur un compte
+    private static void depotSurCompte() {
+        Transaction transaction = new Transaction();
+        System.out.println("Entrez le numéro du compte");
+        int numCompte = sc.nextInt();
+        double montantTransaction;
+        Compte c = compteIsExistt(numCompte);
+        // transaction.setCompte(new Compte(numCompte));
+        // if ( !compteIsExistt(transaction.getCompte())) {
+        if (c == null) {
+            System.out.println("Ce compte n'existe pas");
+            menuTransaction();
+            int numMenu = sc.nextInt();
+            transactionOptions(numMenu);
+            vericationDeLaSaisie(1, 3);
+        } else {
+            transaction.setCompte(c);
+            transaction.setDateTransaction(new Date());
+            System.out.println("Montant à déposer");
+            montantTransaction = sc.nextDouble();
+            System.out.print("Voulez-vous enregistrer ce compte?\t1 : OUI\t2 : NON\n");
+            vericationDeLaSaisie(1, 2);
+            entree = sc.nextInt();
+            if (entree == 1) {
+
+                transaction.setMontantTransaction(montantTransaction);
+                transaction.setNumeroTransaction(genererNumeroTransaction());
+                transaction.setTypeTransaction(TYPE_DEPOT);
+                updateCompte(transaction);
+                System.out.print("Opération enregistrée\n");
+                // menuGenral();
+
+            } else {
+                System.out.print("Opération annulée\n");
+                //menuGenral();
+            }
+            entree = 0;
+
+        }
+    }
+
+    //retrait sur un compte
+    private static void retraitSurCompte() {
+        Transaction transaction = new Transaction();
+        System.out.println("Entrez le numéro du compte");
+        int numCompte = sc.nextInt();
+        double montantTransaction;
+        Compte c = compteIsExistt(numCompte);
+        //transaction.setCompte(new Compte(numCompte));
+        if (c == null) {
+            System.out.println("Ce compte n'existe pas");
+            menuTransaction();
+            int numMenu = sc.nextInt();
+            transactionOptions(numMenu);
+            vericationDeLaSaisie(1, 3);
+        } else {
+            transaction.setCompte(c);
+            System.out.println("Montant à retirer");
+            montantTransaction = sc.nextDouble();
+            System.out.print("Voulez-vous enregistrer ce compte?\t1 : OUI\t2 : NON\n");
+            vericationDeLaSaisie(1, 2);
+            entree = sc.nextInt();
+            if (entree == 1) {
+                transaction.setDateTransaction(new Date());
+                transaction.setMontantTransaction(montantTransaction);
+                transaction.setNumeroTransaction(genererNumeroTransaction());
+                transaction.setTypeTransaction(TYPE_RETRAIT);
+                updateCompte(transaction);
+                System.out.print("Opération enregistrée\n");
+                // menuGenral();
+
+            } else {
+                System.out.print("Opération annulée\n");
+                //menuGenral();
+            }
+            entree = 0;
+
+        }
+    }
+
+    //verification du compte dans la liste
+    /* public static boolean compteIsExist(Compte c) {
+        for (Compte cc : listeCompte) {
+            if (cc.getNumeroCompte() == c.getNumeroCompte()) {
+                return true;
+            }
+        }
+        return false;
+    }*/
+    //ajout de transaction
+    public static void addTransaction(Transaction t) {
+        listeTransactions.add(t);
+
+    }
+
+    //Mise à jour des comptes
+    public static void updateCompte(Transaction t) {
+        //mise à jour du compte 
+        for (Compte cc : listeCompte) {
+            if (cc.getNumeroCompte() == t.getCompte().getNumeroCompte()) {
+                switch (t.getTypeTransaction()) {
+                    case "depot":
+                        System.out.println("------- déposer un montant de "
+                                + t.getMontantTransaction() + " Euros pour le compte "
+                                + t.getCompte().getNumeroCompte()
+                                + " au " + t.getDateTransaction());
+
+                        addTransaction(t);
+                        cc.setSoldeCompte(cc.getSoldeCompte()
+                                + t.getMontantTransaction());
+                        break;
+                    case "retrait":
+                        System.out.println("------- retirer un montant de "
+                                + t.getMontantTransaction() + " Euros pour le compte "
+                                + t.getCompte().getNumeroCompte()
+                                + " au " + t.getDateTransaction());
+
+                        if (cc.getSoldeCompte() >= t.getMontantTransaction()) {
+
+                            addTransaction(t);
+                            cc.setSoldeCompte(cc.getSoldeCompte()
+                                    - t.getMontantTransaction());
+                            break;
+                        }
+                        System.out.println("Le solde du compte est insuffisant "
+                                + "pour faire ce retrait ");
+                        break;
+                }
+                break;
+            }
+        }
+    }
+
+    //Liste de compte par titulaire
+    public static void listCompteParTitulaire() {
+        System.out.print("\n\n~~~RECHERCHER COMPTES D'UN TITULAIRE ~~~\n\n");
+        System.out.println("Entrer le numéro d'identification du titulaire");
+        int numeroIDTitulaire = sc.nextInt();
+        TitulaireCompte ti = titulaireIsExist(numeroIDTitulaire);
+        if (ti != null) {
+            System.out.print("_______INFOS TITULAIRE_____ : \n");
+            System.out.println("numéro =" + ti.getNumeroIdTitulaire()
+                    + "nom =" + ti.getNomTitulaire());
+
+//                System.out.println("numero =" + ti.getNumeroIdTitulaire());
+            int nbCmpt = 0;
+            for (Compte cc : listeCompte) {
+                if (ti.getNumeroIdTitulaire() == cc.getTitulaireCompte().getNumeroIdTitulaire()) {
+
+                    System.out.println("Nom titulaire : " + cc.getTitulaireCompte().getNomTitulaire()
+                            + " ; le numero d'identification = " + cc.getTitulaireCompte().getNumeroIdTitulaire()
+                            + "Le numero de compte = " + cc.getNumeroCompte());
+                    nbCmpt++;
+                }
+            }
+        } else {
+            System.out.println("Désolé ce compte n'existe pas !!");
+        }
+    }
+
+    //Consulter solde compte
+    public static void consulterCompte() {
+        System.out.print("\n\n~~~CONSULTER SOLDE COMPTE~~~\n\n");
+        System.out.println("Numéro du compte à consulter: ");
+        int numCompte = sc.nextInt();
+        Compte cpt = compteIsExistt(numCompte);
+        if (cpt == null) {
+            System.out.println("Ce compte n'existe pas");
+        } else {
+            System.out.println("Le compte numéro =" + cpt.getNumeroCompte()
+                    + "; Titulaire nom= " + cpt.getTitulaireCompte().getNomTitulaire()
+                    + "; Solde= " + cpt.getSoldeCompte());
+        }
+
+    }
+
+    //Rapport comptes
+    public static void rapportCompte() {
+        System.out.print("\n\n~~~RAPPORT SUR LES COMPTES~~~\n\n");
+        for (Compte cc : listeCompte) {
+            System.out.println("___________________________________________________________________________________________");
+            System.out.println("Le numero de compte = " + cc.getNumeroCompte()
+                    + "; Nom titulaire : " + cc.getTitulaireCompte().getNomTitulaire()
+                    + " ; solde = " + cc.getSoldeCompte());
+            List<Transaction> l = transactionsByCompte(cc.getNumeroCompte());
+            if (l != null && !l.isEmpty()) {
+                for (Transaction tr : l) {
+                    System.out.println("Transaction = "
+                            + tr.getTypeTransaction() + " de " + tr.getMontantTransaction() + "euros le " + tr.getDateTransaction());
+                }
+            } else {
+                System.out.println("Aucune transaction éffectuée");
+            }
+
+        }
+
+    }
+
+    //Calcul intérêt sur compte et update compte
+    public static void calculInteret() {
+        System.out.print("\n\n~~~CALCUL D'INTERETS SUR LES COMPTES~~~\n\n");
+        for (Compte cpt : listeCompte) {
+            /* double account = cpt.getSoldeCompte() + ((cpt.getSoldeCompte()
+                    * cpt.getTauxInteret()) / 100);
+            cpt.setSoldeCompte(account);
+             */
+            cpt.setSoldeCompte(cpt.getSoldeCompte() + cpt.getSoldeCompte() * cpt.getTauxInteret() * 0.01);
+            System.out.println("Le numero de compte = " + cpt.getNumeroCompte()
+                    + "; Nom titulaire : " + cpt.getTitulaireCompte().getNomTitulaire()
+                    + " ; le numero d'identification = " + cpt.getTitulaireCompte().getNumeroIdTitulaire()
+                    + " ; le montant après TI= " + cpt.getSoldeCompte()
+                    + " et le taux d'interet = " + cpt.getTauxInteret() + "%");
+        }
+
+    }
 }
